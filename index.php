b8978d192a945b11d39f94a35415474f94d22655
mapro84 <mapro84@gmail.com>
2023-02-06 22:00:17 +0100

    .

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL); //E_ALL
 
 const ROOTDIR = __DIR__;
-require_once(ROOTDIR."/.env");
 require_once(ROOTDIR."/vendor/autoload.php");
+require_once(ROOTDIR."/src/.env");
 require_once(ROOTDIR."/src/app/bootstrap.php");
 ?>
 <link rel="shortcut icon" href="/skills/public/img/favicon.ico">
